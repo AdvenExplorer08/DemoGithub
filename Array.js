587dfa7a0cab8ae1e0d0d7b5d822ac6dc483774a
Kumar Shubham <shubhu.mahii@gmail.com>
2022-05-04 12:48:59 +0530

    First commit

---
+++ Array.js
@@ -0,0 +1,14 @@
+let numbers = new Array(2,5,9,6);
+
+console.log(numbers);
+
+let fruits = ['apples', 'pears','grapes', 'Orange'];
+fruits.push('Mangoes');
+fruits.pop('grapes');
+fruits.unshift('Strawberry');
+
+
+console.log(fruits);
+
+console.log(fruits.indexOf('pears'));
+
