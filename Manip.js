eb33f8ca6a79fd2b464b51a3489d1aced4d6fbb5
Kumar Shubham <shubhu.mahii@gmail.com>
2022-05-05 20:24:17 +0530

    Dom getElementById

---
+++ Manip.js
@@ -0,0 +1,13 @@
+console.log(document.domain);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all[10]);
+console.log(document.forms);
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+headerTitle.textContent = " Namastey Bhaiya, Welcome to Kumar Parth's Website ";
+header.style.borderBottom ='solid 3px #000';
