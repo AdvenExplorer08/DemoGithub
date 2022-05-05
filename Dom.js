155d47f10e4e0257f293e30ac1fb0e42e88ab352
Kumar Shubham <shubhu.mahii@gmail.com>
2022-05-05 22:04:47 +0530

    Get Element by class

--- Dom.js
+++ Dom.js
@@ -1,5 +1,2 @@
-const ul = document.querySelector('.items');
-
-ul.firstElementChild.style.background= 'green';
-ul.children[1].style.background= 'Yellow';
+console.log(document.domain);
 
--- prac.js
+++ prac.js
@@ -7,3 +7,6 @@ let personDetails = `My name is ${name},age is ${age},address is ${address} `;
 console.log(personDetails);
 
 console.log(personDetails.split(','));
+
+console.log(" I LOVE GIT HUB");
+console.log(" I am awesome");
