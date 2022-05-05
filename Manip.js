15db4d7475699cff87f6256d4c74990910765976
Kumar Shubham <shubhu.mahii@gmail.com>
2022-05-05 22:06:44 +0530

    Get

--- Manip.js
+++ Manip.js
@@ -11,3 +11,12 @@ var header = document.getElementById('main-header');
 
 headerTitle.textContent = " Namastey Bhaiya, Welcome to Kumar Parth's Website ";
 header.style.borderBottom ='solid 3px #000';
+
+// getElementsByClassName 
+var items = document.getElementsByClassName('list-group-item');
+
+items[1].style.fontWeight ='bold';
+items[2].style.fontWeight ='bold';
+items[3].style.fontWeight ='bold';
+items[0].style.fontWeight ='bold';
+items[2].style.backgroundColor= ' green'
